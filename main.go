b633ff7bc09d94286c5d0c6da3d4ee9c2e8a1140
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-11-17 23:20:34 +0100

    Added a mean function

--- main.go
+++ main.go
@@ -34,12 +34,34 @@ func (data schemas) display() string {
 	return data.name
 }
 
+var myArray [5]int
+
+func init() {
+	// Initialize the array with values
+	myArray = [5]int{5, 2, 3, 10, 5}
+
+}
+
+func mean(array []int) float64 {
+	var addition = 0
+
+	for i := 0; i < len(array); i++ {
+		value := array[i]
+
+		addition = addition + value
+
+	}
+	// Convert to float64 before division
+	return float64(addition) / float64(len(array))
+}
+
 func main() {
 	clock := time.Now()
 	fmt.Println("Hello World", clock)
 	fmt.Println(add(5, 4))
 	fmt.Println(subtract(2, 4))
 	fmt.Println(named("Emmanuel"))
+	fmt.Println(mean(myArray[:]))
 
 	person1 := schemas{name: "PROGRAMMED"}
 
