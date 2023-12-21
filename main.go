a1370e522ca1188d39bd7552d0ae7f6a9a94e65e
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-12-21 16:45:52 +0100

    restructure: restructured options into functions

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"strconv"
+	"log"
+	"time"
+
+	// "time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,52 +15,70 @@ import (
 // 	name string
 // }
 
-// func displayHeader() string {
-// 	message := fmt.Sprintf("AMOUNT  CATEGORY  DATE        DESCRIPTION")
+// Format and Display Expenses
+func display(id int, amount int, category string, date string, description string) string {
+	message := fmt.Sprintf("%d    N%d   %s  %s   %s", id, amount, category, date, description)
 
-// 	return message
-// }
+	return message
+}
 
-// func display(amount int, category string, date string, description string) string {
-// 	message := fmt.Sprintf("N%d   %s  %s   %s", amount, category, date, description)
+// Add Expenses
+func addExpense(database *sql.DB, err error) {
+	fmt.Println("What's the amount")
 
-// 	return message
-// }
+	var amount int
 
-// var myArray []int
+	fmt.Scan(&amount)
 
-// // Initialize the array with values
-// func init() {
+	fmt.Println("What category is the expense")
 
-// 	myArray = []int{6, 9, 3, 10, 7, 2, 7}
+	var category string
 
-// }
+	fmt.Scan(&category)
 
-// func mean(array []int) float64 {
-// 	var addition int
+	fmt.Println("Expense Description")
 
-// 	for i := 0; i < len(array); i++ {
-// 		value := array[i]
+	var description string
 
-// 		addition = addition + value
+	fmt.Scan(&description)
 
-// 	}
-// 	// Convert to float64 before division
-// 	return float64(addition) / float64(len(array))
-// }
+	var date = time.Now()
+
+	// Format the date in a human-readable way
+	currentDate := date.Format("2006-01-02")
+
+	var statement, _ = database.Prepare("INSERT INTO expense (amount, category, description, currentDate) VALUES(?,?,?,?)")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	statement.Exec(amount, category, description, currentDate)
+	fmt.Println("Expense added succesfully")
+}
+
+// Get All Expenses
+func getAllExpenses(rows *sql.Rows, id int, amount int, category string, description string, currentDate string) {
+	fmt.Println("ID   AMOUNT CATEGORY       DATE         DESCRIPTION")
+
+	fmt.Println()
+
+	rows.Next()
+	{
+		rows.Scan(&id, &amount, &category, &description, &currentDate)
+
+		fmt.Println(display(id, amount, category, currentDate, description))
+	}
+}
 
 func main() {
 	database, _ := sql.Open("sqlite3", "./tracker.db")
 
 	database.SetMaxOpenConns(1)
 
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS expense (id INTEGER PRIMARY KEY, amount INTEGER, category TEXT, description TEXT, currentDate TEXT)")
+	var statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS expense (id INTEGER PRIMARY KEY, amount INTEGER, category TEXT, description TEXT, currentDate TEXT)")
 
 	statement.Exec()
-
-	statement, _ = database.Prepare("INSERT INTO expense (amount, category, description, currentDate) VALUES(?,?,?,?)")
-
-	statement.Exec(100, "testCategory2", "testDescription2", "2023-12-22")
 
 	rows, _ := database.Query("SELECT id, amount, category, description, currentDate FROM expense")
 
@@ -67,44 +88,33 @@ func main() {
 	var description string
 	var currentDate string
 
-	for rows.Next() {
-		rows.Scan(&id, &amount, &category, &description, &currentDate)
-		fmt.Println(strconv.Itoa(id) + ": " + strconv.Itoa(amount) + " " + category + " " + description + " " + currentDate)
+	var command, verb string
+	// defer statement.Exec(amount, category, description, &currentDate)
+
+	fmt.Println("Welcome to Expense Tracker CLI V0.0.1")
+	fmt.Println()
+
+	fmt.Println("add the option -help for instructions e.g tracker -help")
+
+	fmt.Println()
+
+	fmt.Scan(&command, &verb)
+
+	if command == "tracker" && verb == "-help" {
+		fmt.Println()
+		fmt.Println("option -ls for listing all the expenses")
+		fmt.Println()
+		fmt.Println("option -add to add new expenses")
+		fmt.Println()
+		fmt.Println("option -clear to delete all records in expense db")
+		fmt.Println()
+	} else if command == "tracker" && verb == "-ls" {
+		getAllExpenses(rows, id, amount, category, description, currentDate)
+
+	} else if command == "tracker" && verb == "-add" {
+		addExpense(database, err)
+	} else {
+		fmt.Println("Command not found. try 'tracker -help' for instructions")
 	}
-
-	// fmt.Println("Welcome to Expense Tracker CLI V0.0.1")
-	// fmt.Println()
-
-	// fmt.Println("What is the amount")
-
-	// var amount int
-
-	// fmt.Scan(&amount)
-
-	// fmt.Println("What category is the expense")
-
-	// var category string
-
-	// fmt.Scan(&category)
-
-	// fmt.Println("Expense Description")
-
-	// var description string
-
-	// fmt.Scan(&description)
-
-	// var date = time.Now()
-
-	// // Format the date in a human-readable way
-	// currentDate := date.Format("2006-01-02")
-
-	// fmt.Println(displayHeader())
-
-	// fmt.Println()
-
-	// fmt.Println(display(amount, category, currentDate, description))
-
-	// clock := time.Now()
-	// fmt.Println("Hello World", clock)
 
 }
